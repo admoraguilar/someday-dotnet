cbf8ae90e107fd252e30c7b06f324519ba501992
Admor Aloysious Aguilar <admor.aguilar@gmail.com>
2021-04-02 13:02:46 +0800

    * Initial commit

---
+++ Someday.ConsoleUI/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Someday.SDK;
+
+namespace Someday.ConsoleUI
+{
+	public class Program
+	{
+		static async Task Main(string[] args)
+		{
+			ImagesClient imagesClient = new ImagesClient();
+			List<string> results = await imagesClient.GetImagesAsync();
+			foreach(string result in results) {
+				Console.WriteLine($"Images got: {result}");
+			}
+		}
+	}
+}
---
+++ Someday.SDK/Common/HttpClientFactory.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+
+namespace Someday.SDK
+{
+	internal static class HttpClientFactory
+	{
+		private static readonly string defaultHttpClientKey = "0";
+		private static Dictionary<string, HttpClient> httpClientsDict = new Dictionary<string, HttpClient>();
+
+		static HttpClientFactory()
+		{
+			Set(defaultHttpClientKey, new HttpClient());
+		}
+
+		public static HttpClient Get()
+		{
+			return httpClientsDict[defaultHttpClientKey];
+		}
+
+		public static bool TryGet(string key, out HttpClient client)
+		{
+			return httpClientsDict.TryGetValue(key, out client);
+		}
+
+		public static HttpClient Set(string key, HttpClient client)
+		{
+			if(TryGet(key, out HttpClient existingClient)) { return existingClient; }
+			return httpClientsDict[key] = client;
+		}
+	}
+}
